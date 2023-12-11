bae67232e1d1f93542d6237b03f9a5f0591335fb
Аркадий <chuark@gmail.com>
2023-12-11 08:31:22 +0300

    - reworked test tox>nox

---
+++ noxfile.py
@@ -0,0 +1,17 @@
+import nox
+
+
+@nox.session(python="3.8")
+def lint(session: nox.Session) -> None:
+    """Run pre-commit hooks on all files in the repository."""
+    session.install("pre-commit")
+    session.run("pre-commit", "install")
+    session.run("pre-commit", "run", "-a")
+
+
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
+def tests(session: nox.Session) -> None:
+    """Run tests in different Django versions"""
+    for version in range(2):
+        session.install(f"Django>=4.{version + 1},<4.{version + 2}")
+        session.run("python", "-m", "unittest", "discover", "-s", "tests")
